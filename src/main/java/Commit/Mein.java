fb40473d365fd5e08cd229c538275cd96162e93e
Santiago <jefferson02830@gmail.com>
2022-08-21 07:27:45 -0500

    Commit de Jefferson Pinchao

---
+++ src/main/java/Commit/Mein.java
@@ -0,0 +1,7 @@
+package Commit;
+
+public class Mein {
+    public static void main(String[] args) {
+        System.out.println("Primer codigo y commit de:\nJefferson Santiago Pinchao\n");
+    }
+}
